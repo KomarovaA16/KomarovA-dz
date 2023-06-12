9a42f83b6eeede5c8467b2a2de8c6a91ee7e9ee7
User <komarovvva16@gmail.com>
2023-06-13 01:05:27 +0500

    task3

---
+++ UGI-216004-main + task3/PhotoEnhancer/PhotoEnhancer/Filters/GammaСolorСorrectionParameters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoEnhancer
+{
+    public class GammaСolorСorrectionParameters : IParameters
+    {
+        [ParameterInfo(Name = "Коэффициент для канала R",
+                    MinValue = 0.2,
+                    MaxValue = 5,
+                    DefaultValue = 1,
+                    Increment = 0.01)]
+        public double CoefficientR { get; set; }
+
+        [ParameterInfo(Name = "Коэффициент для канала G",
+                    MinValue = 0.2,
+                    MaxValue = 5,
+                    DefaultValue = 1,
+                    Increment = 0.01)]
+        public double CoefficientG { get; set; }
+
+        [ParameterInfo(Name = "Коэффициент для канала B",
+                    MinValue = 0.2,
+                    MaxValue = 5,
+                    DefaultValue = 1,
+                    Increment = 0.01)]
+        public double CoefficientB { get; set; }
+    }
+}
